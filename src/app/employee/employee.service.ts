1425c85017a19bc6e25414c234db17e02d846dac
Raimundo Neto <rasneto147@gmail.com>
2022-11-11 21:56:14 -0300

    create employee ready

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -2,17 +2,54 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class EmployeeService {
   constructor(private prisma: PrismaService) {}
 
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    const data = {
+      ...createEmployeeDto,
+      password: await bcrypt.hash(createEmployeeDto.password, 10)
+    }
+
+    const employeeExist = await this.prisma.employee.findFirst({
+      where: {
+        email: data.email
+      }
+    })
+
+    if(employeeExist) {
+      throw new Error("this email already exists");
+    }
+
+    await this.prisma.employee.create({
+      data: {
+        name: data.name,
+        lastname: data.lastname,
+        email: data.email,
+        password: data.password
+      }
+    });
+
+    return {messege: 'employee reguster successfully'}
   }
 
   async findAll() {
-    const employees = await this.prisma.employee.findMany();
+    const employees = await this.prisma.employee.findMany({
+      select: {
+        createdAt: true,
+        updatedAt: true,
+        name: true,
+        lastname: true,
+        email: true,
+        role: true
+      },
+      orderBy: {
+        id: 'desc'
+      }
+    });
     return employees;
   }
 
