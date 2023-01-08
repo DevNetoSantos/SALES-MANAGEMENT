7165f396ecb2aef52d09a87d71914247da09cba3
Raimundo Neto <rasneto147@gmail.com>
2023-01-07 23:32:59 -0300

    mock ready client

--- src/app/client/client.service.spec.ts
+++ src/app/client/client.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../database/prisma.service';
 import { ClientService } from './client.service';
 
+const fakeClient = [
+  {
+    id: 1,
+    name: 'client1',
+    cpf: '10000000000'
+  },
+  {
+    id: 2,
+    name: 'client2',
+    cpf: '20000000000'
+  },
+  {
+    id: 3,
+    name: 'client3',
+    cpf: '30000000000'
+  }
+]
+
+const prismMock = {
+  client: {
+    create: jest.fn().mockReturnValue(fakeClient[0]),
+    findMany: jest.fn().mockResolvedValue(fakeClient),
+    findUnique: jest.fn().mockResolvedValue(fakeClient[0]),
+    update: jest.fn().mockResolvedValue(fakeClient),
+    delete: jest.fn().mockReturnValue(undefined) // O método delete não retorna nada
+  }
+}
+
 describe('ClientService', () => {
-  let service: ClientService;
+  let clientService: ClientService;
+  let clientRepository: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientService],
+      providers: [ClientService, {provide: PrismaService, useValue: {prismMock}}],
     }).compile();
 
-    service = module.get<ClientService>(ClientService);
+    clientService = module.get<ClientService>(ClientService);
+    clientRepository = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(clientService).toBeDefined();
+  });
+
+  it('shoudl be defined', () => {
+      expect(clientRepository).toBeDefined();
   });
 });
--- src/app/client/client.service.ts
+++ src/app/client/client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/database/prisma.service';
+import { PrismaService } from '../../database/prisma.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 
